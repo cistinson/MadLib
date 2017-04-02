c97cfd40e803a6462e607afdefc2b2df83cdbd2c
cistinson <cistinson@lcmail.lcsc.edu>
2017-04-01 19:04:17 -0700

    updated story

--- Madlib2.js
+++ Madlib2.js
@@ -22,13 +22,15 @@ function libbin(){
   var pro4 = document.getElementById("pro4").value;
   var pro5 = document.getElementById("pro5").value;
   var pro6 = document.getElementById("pro6").value;
+  var pro7 = document.getElementById("pro7").value;
+  var pro8 = document.getElementById("pro8").value;
   var noun1 = document.getElementById("noun1").value;
   var noun2 = document.getElementById("noun2").value;
-  var noun3 = document.getElementById("noun3").value;
-  var noun4 = document.getElementById("noun4").value;
-  var noun5 = document.getElementById("noun5").value;
+//  var noun3 = document.getElementById("noun3").value;
+//  var noun4 = document.getElementById("noun4").value;
+//  var noun5 = document.getElementById("noun5").value;
   var verb1 = document.getElementById("verb1").value;
-  var verb2 = document.getElementById("verb2").value;
+//  var verb2 = document.getElementById("verb2").value;
   var adj1 = document.getElementById("adj1").value;
   var adj2 = document.getElementById("adj2").value;
   var adj3 = document.getElementById("adj3").value;
@@ -38,7 +40,7 @@ function libbin(){
   var adj7 = document.getElementById("adj7").value;
   var adj8 = document.getElementById("adj8").value;
   
-  madLib.innerHTML ="There once was a girl named " + pro1 + ". She was very " + adj1 + " " + adj2 + " " + ", and " + adj3 + ". When she walked around " + pro2 + ", people often looked at her funny. It was probably due to her " + noun1 + " inch nose or her " + adj4 + " smell. She tried hard in school and managed a GPA of " + noun2 +", which everyone told her was very " + adj5 + ". She had a pet named " + pro3 + " who liked to " + verb1 + " and urinate " + verb2 + " while " + pro1 + " was working. Both " + pro1 + " and " + pro3 + " had the same IQ of " + noun3 + ". She tried hard to be as " + adj6 + " and " + adj7 + " as her older brother " + pro4 + ", but it was an impossible task for he was the " + adj8 + "est man alive. He was " + noun4 + " inches tall with an IQ of " + noun5 + ". He had the jawline of " + pro5 + " and the hair of " + pro6 + ". He was undoubtedly the favorite child. :-)";
+  madLib.innerHTML = "Hello, my name is " + pro1 + ". I drive a " + pro2 + ". When people see me driving around town, they often give me " + adj1 + " looks, but it’s probably just because they feel so much " + pro3 + " about my ride. I’ve named him " + pro4 + " and he’s my #1 love. He’s got " + noun1 + " inch rims that sparkle " + pro5 + " in the sunlight. His engine is " + adj2 + " and " + adj3 + " - it makes a " + adj4 + " sound when it starts up, which causes my " + pro6 + " to tingle. Even though " + pro4 + " has been good to me, I can’t help but dream of something better, like a  " + pro7 + ". " + pro7 + " makes the best rides. They are " + adj5 + ", " + adj6 + ", and they " + verb1 + " so smoothly. Maybe someday I’ll get up the courage to leave " + pro4 + ", but it’s just so " + adj7 + ". It doesn’t help that every time I ride in him my wiener shrinks " + noun2 + " inches, which makes me feel" + pro8 + ". I think it’s time to make a change, to the Ford dealership I go!<br /><br /><img src='http://c.quoteson.net/93/4644369-chevy-truck-funny-quotes.jpg' />";
 
   event.preventDefault();
 }
